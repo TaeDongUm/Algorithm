bf452e9864f36385d2cc5b39a6839e770ad0b82a
TaeDongUm <etd93@naver.com>
2023-05-28 22:27:45 +0900

    5_28 algorithm study

---
+++ Algorithm_study/src/study_5.java
@@ -0,0 +1,9 @@
+
+public class study_5 {
+	public static void main(String[] args) {
+		//int[] a;
+		int a[];
+		int b;
+		a= new int[5];
+	}
+}
--- Algorithm_study/src/test.java
+++ Algorithm_study/src/test.java
@@ -1,9 +1,36 @@
-public class test {
+import java.util.Scanner;
+//public class test {
+//
+//	public static void main(String[] args) {
+//		for (int i =0; i<2;i++) {
+//			System.out.print("abcs");
+//			System.out.println();
+//		}
+//	}
+//}
 
+class test{
+	static int maxOf(int[] a) {
+		int max= a[0];
+		for (int i=1; i <a.length; i++) 
+			if (a[i] > max) 
+				max = a[i];
+		return max;
+		
+	}
 	public static void main(String[] args) {
-		for (int i =0; i<2;i++) {
-			System.out.print("abcs");
-			System.out.println();
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("asbsd");
+		System.out.println();
+		
+		int num = stdIn.nextInt();
+		
+		int[] height = new int[num];
+		
+		for (int i =0;i< num; i++) {
+			System.out.print("height[" + i + "]");
+			height[i] = stdIn.nextInt();
 		}
+		System.out.println("최댓값은" + maxOf(height) + "입니다.");
 	}
 }
