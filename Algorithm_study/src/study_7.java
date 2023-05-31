82b7cd063367d5d4e2dbf3a98ff971f328138412
TaeDongUm <etd93@naver.com>
2023-05-31 21:18:29 +0900

    5_31 algorithm study

---
+++ Algorithm_study/src/study_7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class study_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+      
+        System.out.println(String.format("%.3f",a));
+        System.out.println(String.format("%.3f",b));
+        System.out.println(String.format("%.3f",c));
+    }
+	}
+
+
