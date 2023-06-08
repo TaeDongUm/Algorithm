7aeb46d416788317a910e3fc548e8e4ebb960a89
TaeDongUm <etd93@naver.com>
2023-06-08 16:25:28 +0900

    6_8 algorithm study

--- Algorithm_study/src/study_13.java
+++ Algorithm_study/src/study_13.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-public class study_12{
+public class study_13{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        a +=8;
-        b *=3;
-        System.out.println(a + "\n"+ b+"\n"+(a*b));
-
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
 	}
 
 }
