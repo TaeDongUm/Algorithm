e4e7ac4297125b3561eedd21b34fc93fee73cd1b
TaeDongUm <etd93@naver.com>
2023-06-09 23:47:04 +0900

    6_9  algorithm study

---
+++ Algorithm_study/src/study_14.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class study_14{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + " * " + b + " = " + a * b);
+        System.out.println(a + " / " + b + " = " + a / b);
+
+}
+}
