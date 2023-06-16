24f869a50359714c95a865731dfbec9ec7353c18
TaeDongUm <etd93@naver.com>
2023-06-16 23:34:01 +0900

    6_16 algorithm study

--- Algorithm_study/src/study_16.java
+++ Algorithm_study/src/study_16.java
@@ -52,3 +52,15 @@ class MaxminusMin {
             System.out.println(a - b);
 	    }
 	}
+class IfElseUse{
+	void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		if(a>=80) {
+			System.out.println("pass");
+		}
+		else {
+			System.out.println(80-a + " more score");
+		}
+	}
+}
