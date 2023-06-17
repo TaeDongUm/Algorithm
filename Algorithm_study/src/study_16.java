ba6ced3191b05b35d52ee426eba5471f81060cf0
TaeDongUm <etd93@naver.com>
2023-06-17 23:42:11 +0900

    6_17 algorithm study

--- Algorithm_study/src/study_16.java
+++ Algorithm_study/src/study_16.java
@@ -64,3 +64,13 @@ class IfElseUse{
 		}
 	}
 }
+class Ternary{
+	void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int max;
+		max = a>b ? a:b;
+		System.out.println(max);
+	}
+}
