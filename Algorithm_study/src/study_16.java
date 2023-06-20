d5b79a6553e17d4a94d5b58b847ef90534fb4edd
TaeDongUm <etd93@naver.com>
2023-06-20 23:24:01 +0900

    6_20 algorithm study

--- Algorithm_study/src/study_16.java
+++ Algorithm_study/src/study_16.java
@@ -110,3 +110,24 @@ class CallAttendence {
 		}
 	}
 }
+class Score{
+	void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a =sc.nextInt();
+        if(a>=90){
+            System.out.println("A");
+        }
+        else if(a>=80){
+            System.out.println("B");
+        }
+        else if(a>=70){
+            System.out.println("C");
+        }
+        else if (a>=60){
+            System.out.println("D");
+        }
+        else{
+            System.out.println("F");
+        }
+	}
+}
