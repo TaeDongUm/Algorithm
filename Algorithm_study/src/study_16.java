15ba8d644a4519dedc1ea25ce002acd72997a5d8
TaeDongUm <etd93@naver.com>
2023-06-22 23:33:36 +0900

    6_22 algorithm study

--- Algorithm_study/src/study_16.java
+++ Algorithm_study/src/study_16.java
@@ -131,3 +131,23 @@ class Score{
         }
 	}
 }
+class ThreeValue {
+	void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if(a<=b && a<=c) {
+			System.out.print(1+" ");
+		}
+		else {
+			System.out.print(0+" ");
+		}
+		if(a==b && a ==c) {
+			System.out.print(1+" ");
+		}
+		else {
+			System.out.print(0+" ");
+		}
+	}
+}
