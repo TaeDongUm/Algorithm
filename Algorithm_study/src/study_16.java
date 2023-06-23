31aef35e5cfd7e24564d3e7188af3295837893c6
TaeDongUm <etd93@naver.com>
2023-06-23 23:41:57 +0900

    6_23 algorithm study

--- Algorithm_study/src/study_16.java
+++ Algorithm_study/src/study_16.java
@@ -151,3 +151,19 @@ class ThreeValue {
 		}
 	}
 }
+class VisionTest {
+	void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        if (a>=1.0 && b>=1.0){
+            System.out.println("High");
+        }
+        else if(a>=0.5 && b>=0.5){
+            System.out.println("Middle");
+        }
+        else {
+            System.out.println("Low");
+        }
+	}
+}
