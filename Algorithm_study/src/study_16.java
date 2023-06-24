fc0171f11b270adc9f30e4d1b758387b587b383f
TaeDongUm <etd93@naver.com>
2023-06-24 23:29:49 +0900

    6_24 algorithm study

--- Algorithm_study/src/study_16.java
+++ Algorithm_study/src/study_16.java
@@ -167,3 +167,15 @@ class VisionTest {
         }
 	}
 }
+class AndOf {
+	void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>=10 && a<=20){
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("no");
+        }
+}
+}
