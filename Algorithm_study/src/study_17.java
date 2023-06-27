f404c7360fb2caf8e61782d539491225c9d0749a
TaeDongUm <etd93@naver.com>
2023-06-27 23:28:55 +0900

    6_27 algorithm study

---
+++ Algorithm_study/src/study_17.java
@@ -0,0 +1,43 @@
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class study_17{
+
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = "";
+        while ((s=br.readLine()) !=null)
+        {
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        bw.write(a+b+"\n");
+        bw.flush();
+        bw.close();
+        }
+	}
+	
+}
+
+class AndOr {
+	void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String s = br.readLine();
+		int n = Integer.parseInt(s);
+		if((n%2==1 && n%3==0) || (n%2==0 && n%5==0)){
+			bw.write("true");
+		}
+		else {
+			bw.write("false");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
