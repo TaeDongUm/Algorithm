da1051c747f9ff6b914abe77d6f2d792272b11cd
TaeDongUm <etd93@naver.com>
2023-07-07 23:24:13 +0900

    7_7 algorithm study

---
+++ Algorithm_study/src/study_20.java
@@ -0,0 +1,31 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.lang.Math;
+import java.io.BufferedReader;
+
+// java EOF
+// 1. scanner hasNexxt()
+public class study_20{
+	
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			System.out.print(sc.nextInt()+" ");
+		}
+	}
+}
+
+// 2. BufferedReader, input = br.readLine()
+
+class EOF2{
+	void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = "";
+		while((line = br.readLine()) != null) {
+			// ~~~~
+		}
+	}
+}
+
+
