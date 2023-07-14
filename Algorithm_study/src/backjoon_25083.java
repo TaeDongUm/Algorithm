cfebe2ecc447ef76e06b1fc0139dd3e57d13325e
TaeDongUm <etd93@naver.com>
2023-07-14 22:04:10 +0900

    7_14 algorithm study

---
+++ Algorithm_study/src/backjoon_25083.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class backjoon_25083 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("         ,r\'\"7");
+		System.out.println("r`-_   ,\'  ,/");
+		System.out.println(" \\. \". L_r\'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+	}
+
+}
