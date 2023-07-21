58df6753ceea6464ca0b89285f5f7ac31eb83b65
TaeDongUm <etd93@naver.com>
2023-07-21 23:46:57 +0900

    7_21 algorithm study

---
+++ Algorithm_study/src/backjoon_2566.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class backjoon_2566 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[][] Matrix = new int[9][9];
+		for(int i =0;i<9;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<9;j++) {
+				Matrix[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		int max=0;
+		int indexR=0;
+		int indexC=0;
+		for(int i=0;i<9;i++) {
+			for(int j=0;j<9;j++) {
+				if(max <=Matrix[i][j]) {
+					max = Matrix[i][j];
+					indexR = i+1;
+					indexC = j+1;
+				}
+			}
+		}
+		System.out.println(max);
+		System.out.println(indexR + " "+ indexC);
+		
+	}
+
+}
