3a82b1dd52e67a4dfe409e8a5587d011cf5ab650
TaeDongUm <etd93@naver.com>
2023-07-23 18:47:34 +0900

    7_23 algorithm study

---
+++ Algorithm_study/src/backjoon_2563.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class backjoon_2563 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] map = new int[101][101];
+		int N = sc.nextInt();
+		for(int i=0;i<N;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			for(int j =b;j<b+10;j++) {
+				for(int k =a;k<a+10;k++) {
+					map[j][k] +=1;
+				}
+			}
+		}
+		int count =0;
+		for(int i=0;i<=100;i++) {
+			for(int j =0;j<=100;j++) {
+				if(map[i][j] !=0) {
+					count +=1;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+
+}
