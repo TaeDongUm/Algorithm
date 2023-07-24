6a5fd636bec174291d79869aeaf2d52bb8b2deb2
TaeDongUm <etd93@naver.com>
2023-07-24 23:46:46 +0900

    7_24 algorithm study

---
+++ Algorithm_study/src/backjoon_11005.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class backjoon_11005 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = Integer.parseInt(sc.next());
+		int num2 = Integer.parseInt(sc.next());
+		String value = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String result ="";
+		while(true) {
+			if(num1 ==0) {
+				break;
+			}
+			int share = num1%num2;
+			result += Character.toString(value.charAt(share));
+			num1 = num1/num2;
+		}
+		StringBuffer sb = new StringBuffer(result);
+		String reverse = sb.reverse().toString();
+		System.out.println(reverse);
+		
+	}
+
+}
