5039156553d5a788dcb579f5665face437050978
TaeDongUm <etd93@naver.com>
2023-08-05 02:17:01 +0900

    8_5 algorithm study

--- Algorithm_study/src/backjoon/backjoon_9205.java
+++ Algorithm_study/src/backjoon/backjoon_9205.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
 import java.util.LinkedList;
@@ -17,7 +18,7 @@ public class backjoon_9205 {
 		int t = Integer.parseInt(br.readLine());
 		for(int i=0;i<t;i++) {
 			List<int[]> list = new ArrayList<>();
-			int n = Integer.parseInt(br.readLine());
+			n = Integer.parseInt(br.readLine());
 			for(int j =0;j<n+2;j++) {
 				st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
@@ -36,26 +37,38 @@ public class backjoon_9205 {
 				}
 				
 			}
-//			if(bfs(list)) {
-//				System.out.println("happy");
-//			}else {
-//				System.out.println("sad");
-//			}
+			if(bfs(list)) {
+				System.out.println("happy");
+			}else {
+				System.out.println("sad");
+			}
 		}
 	}
 	// 임시 commit
-//	static boolean bfs(List<int[]> list) {
-//		Queue<int[]> q = new LinkedList<>();
-//		boolean[] visited = new boolean[n];
-//		q.add(new int[]{sx,sy});
-//		if(!q.isEmpty()) {
-//			int[] position = q.poll();
-//			int a = position[0], b = position[1];
-//			if(Math.abs(a-px)+Math.abs(b-py)<=1000) {
-//				return true;
-//			}
-//			
-//		}
-//	}
+	static boolean bfs(List<int[]> list) {
+		Queue<int[]> q = new LinkedList<>();
+		boolean[] visited = new boolean[n];
+		q.add(new int[]{sx,sy});
+		int i =0;
+		while(!q.isEmpty()) {
+			int[] position = q.poll();
+			int a = position[0], b = position[1];
+			if(Math.abs(a-px)+Math.abs(b-py)<=1000) {
+				return true;
+			}
+			for(int j =0;j<n;j++) {
+				if(!visited[j]) {
+					// convenience store
+					int[] cs= list.get(j);
+					int x = cs[0], y=cs[1];
+					if(Math.abs(a-x)+Math.abs(b-y)<=1000) {
+						q.add(new int[] {x,y});
+						visited[j]=true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 
 }
