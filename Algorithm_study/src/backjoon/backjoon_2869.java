60db29fbdd64e1dc173b0a941b6f54b3b731c315
TaeDongUm <etd93@naver.com>
2023-08-13 22:49:40 +0900

    8_13 algorithm study

--- Algorithm_study/src/backjoon/backjoon_2869.java
+++ Algorithm_study/src/backjoon/backjoon_2869.java
@@ -1,21 +1,16 @@
 package backjoon;
 import java.util.Scanner;
 public class backjoon_2869 {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			int V = sc.nextInt();
+			int cnt =0;
+	        cnt = (V-B)/(A-B);
+	        if(((V-B)%(A-B)) !=0)
+	            cnt++;
+	      	System.out.println(cnt);
+		}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int V = sc.nextInt();
-		int cnt =0;
-		cnt = V/(A-B)-((A/(A-B))-1);
-		
-//		if(((V/A)*B+ V%A)%A !=0) {
-//			cnt +=((V/A)*B+ V%A)/A +1;
-//		}else {
-//			cnt +=((V/A)*B+ V%A)/A;
-//		}
-		System.out.println(cnt);
 	}
-
-}
